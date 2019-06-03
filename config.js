736132c116281213cd8283f6e54e53662de114b9
Patrick Richardson <setfloat@gmail.com>
2019-06-03 00:51:07 -0700

    updated server to ssl

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const endpoint = `http://localhost:4445`;
 // export const prodEndpoint = `https://hausmix-yoga-prod.herokuapp.com/`;
-export const prodEndpoint = `http://api.hausmix.com/`;
+export const prodEndpoint = `https://api.hausmix.com/`;
 // export const perPage = 6; // pagination info
